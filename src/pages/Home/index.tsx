c3c2cce1c840b2c32682e4b56f4070d704d19bc6
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2025-04-29 18:06:24 +0200

    feat: Implement repository fetching and display in Home component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,39 @@
+import { useEffect, useState } from "react"
 import { Header } from "../../components/Header"
 import { Search } from "./components/Search"
 import { HomeContainer, RepositoriesContainer, RepositoryContent, RepositoryTitle, Description } from "./styles"
 
+import { api } from "../../lib/axios"
+
+interface RepositoryProps {
+    id: number;
+    name: string;
+    description: string;
+    updated_at: string;
+}
+
 export function Home() {
+    const [repositories, setRepositories] = useState<RepositoryProps[]>();
+    
+    useEffect(() => {
+        async function fetchRepoProfileData() {
+            const response = await api.get('/users/devnestali/repos?per_page=100');
+            
+            const filteredRepos: RepositoryProps[] = response.data.map((repo: any) => ({
+                id: repo.id,
+                name: repo.name,
+                description: repo.description,
+                updated_at: repo.updated_at
+
+            }))
+
+
+            setRepositories(filteredRepos);
+            
+        }
+
+        fetchRepoProfileData();
+    }, [repositories])
     return (
         <HomeContainer>
             <Header />
@@ -10,96 +41,25 @@ export function Home() {
             <Search />
 
             <RepositoriesContainer>
-                <RepositoryContent>
-                    <RepositoryTitle>
-                        <h1>Javascript data types and data structures</h1>
-                        <time dateTime="2025-04-24">1 dia</time>
-                    </RepositoryTitle>
+                
+                {
+                    repositories && repositories?.length > 0 
+                    ? repositories?.map((repository) => {
+                        return (
+                            <RepositoryContent key={repository.id}>
+                                <RepositoryTitle>
+                                    <h1>{repository.name}</h1>
+                                    <time dateTime="2025-04-24">{repository.updated_at}</time>
+                                </RepositoryTitle>
 
-                    <Description>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam non saepe, sint commodi quis, eaque provident fugiat praesentium totam temporibus voluptatum libero rerum doloremque doloribus! Sequi exercitationem voluptatem delectus numquam?
-                    </Description>
-                </RepositoryContent>
-                <RepositoryContent>
-                    <RepositoryTitle>
-                        <h1>Javascript data types and data structures</h1>
-                        <time dateTime="2025-04-24">1 dia</time>
-                    </RepositoryTitle>
-
-                    <Description>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam non saepe, sint commodi quis, eaque provident fugiat praesentium totam temporibus voluptatum libero rerum doloremque doloribus! Sequi exercitationem voluptatem delectus numquam?
-                    </Description>
-                </RepositoryContent>
-                <RepositoryContent>
-                    <RepositoryTitle>
-                        <h1>Javascript data types and data structures</h1>
-                        <time dateTime="2025-04-24">1 dia</time>
-                    </RepositoryTitle>
-
-                    <Description>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam non saepe, sint commodi quis, eaque provident fugiat praesentium totam temporibus voluptatum libero rerum doloremque doloribus! Sequi exercitationem voluptatem delectus numquam?
-                    </Description>
-                </RepositoryContent>
-                <RepositoryContent>
-                    <RepositoryTitle>
-                        <h1>Javascript data types and data structures</h1>
-                        <time dateTime="2025-04-24">1 dia</time>
-                    </RepositoryTitle>
-
-                    <Description>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam non saepe, sint commodi quis, eaque provident fugiat praesentium totam temporibus voluptatum libero rerum doloremque doloribus! Sequi exercitationem voluptatem delectus numquam?
-                    </Description>
-                </RepositoryContent>
-                <RepositoryContent>
-                    <RepositoryTitle>
-                        <h1>Javascript data types and data structures</h1>
-                        <time dateTime="2025-04-24">1 dia</time>
-                    </RepositoryTitle>
-
-                    <Description>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam non saepe, sint commodi quis, eaque provident fugiat praesentium totam temporibus voluptatum libero rerum doloremque doloribus! Sequi exercitationem voluptatem delectus numquam?
-                    </Description>
-                </RepositoryContent>
-                <RepositoryContent>
-                    <RepositoryTitle>
-                        <h1>Javascript data types and data structures</h1>
-                        <time dateTime="2025-04-24">1 dia</time>
-                    </RepositoryTitle>
-
-                    <Description>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam non saepe, sint commodi quis, eaque provident fugiat praesentium totam temporibus voluptatum libero rerum doloremque doloribus! Sequi exercitationem voluptatem delectus numquam?
-                    </Description>
-                </RepositoryContent>
-                <RepositoryContent>
-                    <RepositoryTitle>
-                        <h1>Javascript data types and data structures</h1>
-                        <time dateTime="2025-04-24">1 dia</time>
-                    </RepositoryTitle>
-
-                    <Description>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam non saepe, sint commodi quis, eaque provident fugiat praesentium totam temporibus voluptatum libero rerum doloremque doloribus! Sequi exercitationem voluptatem delectus numquam?
-                    </Description>
-                </RepositoryContent>
-                <RepositoryContent>
-                    <RepositoryTitle>
-                        <h1>Javascript data types and data structures</h1>
-                        <time dateTime="2025-04-24">1 dia</time>
-                    </RepositoryTitle>
-
-                    <Description>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam non saepe, sint commodi quis, eaque provident fugiat praesentium totam temporibus voluptatum libero rerum doloremque doloribus! Sequi exercitationem voluptatem delectus numquam?
-                    </Description>
-                </RepositoryContent>
-                <RepositoryContent>
-                    <RepositoryTitle>
-                        <h1>Javascript data types and data structures</h1>
-                        <time dateTime="2025-04-24">1 dia</time>
-                    </RepositoryTitle>
-
-                    <Description>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam non saepe, sint commodi quis, eaque provident fugiat praesentium totam temporibus voluptatum libero rerum doloremque doloribus! Sequi exercitationem voluptatem delectus numquam?
-                    </Description>
-                </RepositoryContent>
+                                <Description>
+                                    {repository.description}
+                                </Description>
+                            </RepositoryContent>
+                        )
+                    })
+                    : 'Nao ha repositorios'
+                }
             </RepositoriesContainer>
         </HomeContainer>
     )
